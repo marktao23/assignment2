a3e6ad722be2bbac91a3d8c3d8fbae6c8b7fedf7
TroyBurkhart <77162040+TroyBurkhart@users.noreply.github.com>
2021-07-06 19:44:07 -0400

    Create snake.pl

---
+++ snake.pl
@@ -0,0 +1,71 @@
+
+:- [tests].
+:- use_module(library(clpfd)). % Import the module
+:- set_prolog_flag(clpfd_monotonic, true). % setting to get useful errors sometimes
+
+
+
+
+% checks if valid solution possible and if given first three solve the solution
+
+snake(RowClues, ColClues, Grid, Solution):- copyGrid(Grid,Solution),
+checkRowClues(Solution,RowClues),
+checkColClues(Solution,ColClues),
+nonTouching(Solution) % snake cannot touch itself
+,countNeighbors(Solution) % heads have 1 neighbor ,midpoints 2
+,snakeConnected(Solution) % snake must be connected
+.
+
+
+copyGrid([],[]).
+copyGrid([Row|G],[RowS|S]) :- copyRow(Row,RowS), copyGrid(G,S).
+
+copyRow([],[]).
+copyRow([-1|R],[_|S]) :- copyRow(R,S).
+copyRow([Clue|R],[Clue|S]) :- copyRow(R,S).
+
+
+checkRowClues([], []).
+checkRowClues([X|S], [Y|H]) :- counter(X, N), counter2(X, M), (N + M = Y), checkRowClues([S],[H]).
+
+
+checkColClues([],[]).
+checkColClues(X|S, Y|H) :- counter(X, N), counter2(X, M), (N + M = Y), checkColClues([S],[H]).
+
+
+
+check_neighbors_pattern(0,_,_,_,_).check_neighbors_pattern(Piece,N,E,S,W) :- 1 #=< Piece,count_cell(N,X1),
+count_cell(E,X2),
+count_cell(S,X3),
+count_cell(W,X4),
+Piece #= X1+X2+X3+X4.
+
+check_neighbors_rows([_,N,A3|RowA],[W,M,E|RowB],[_,S,C3|RowC]) :-check_neighbors_pattern(M,N,E,S,W),check_neighbors_rows([N,A3|RowA],[M,E|RowB],[S,C3|RowC]).
+% Extend a row by adding a 0 at both ends
+extend_row(OldRow,NewRow) :- append([0|OldRow],[0],NewRow).
+extend_grid(OldGrid,NewGrid) :-transpose(OldGrid,TransGrid),extend_grid_rows(TransGrid,RowTransGrid),transpose(RowTransGrid,RowGrid),extend_grid_rows(RowGrid,NewGrid).
+
+
+extend_grid_rows([R, L], [NR, NL]) :- extend_grid_rows(L, NL), extend_row(R, NR).
+
+count_cell(0,0).
+count_cell(1,1).
+count_cell(2,1).
+
+
+
+countNeighbors(S) :-
+
+
+
+
+
+
+
+
+% counter code found here at https://stackoverflow.com/questions/32691313/counter-in-prolog
+counter([], 0).
+counter([X|Xs], N) :- dif(X, 1) -> counter(Xs, N); counter(Xs, N0), succ(N0, N)).
+
+counter2([], 0).
+counter2([X|Xs], N) :- dif(X, 2) -> counter(Xs, N); counter(Xs, N0), succ(N0, N)).
